048e184495ab40adda29a58034456ac513423553
mohamed  habib <mohamedhabibjaberi111@gmail.com>
2020-07-22 19:36:26 +0200

    Testing Webservice Client

--- HotCoffeeMVVM/Controllers/OrderTableViewController.swift
+++ HotCoffeeMVVM/Controllers/OrderTableViewController.swift
@@ -11,4 +11,29 @@ import UIKit
 
 class OrderTableViewController: UITableViewController {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+         
+         populateOrders()
+        
+    }
+    
+    private func populateOrders(){
+        
+                guard let coffeeOrdersUrl = URL(string: "Https://guarded-retreat-82533.herokuapp.com/orders") else {
+                    fatalError("URL was not correct")
+                }
+         let ressource = Resource<[Order]>(url: coffeeOrdersUrl)
+        
+        WebService().load(resource: ressource) { [weak self](result) -> (Void) in
+                  
+                  switch result {
+                  case .success(let orders) :
+                      print(orders)
+                      
+                  case .failure(let error) :
+                      print(error)
+                  }
+              }
+    }
 }
